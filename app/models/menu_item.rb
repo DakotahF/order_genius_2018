b1e3fa47ed4e378f5a063880a7c1fc74ad263103
Jason Swett <jason@benfranklinlabs.com>
2018-06-18 09:45:22 -0400

    Add scope.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,6 +3,8 @@ class MenuItem < ApplicationRecord
   has_many :order_menu_items
   validates :name, uniqueness: { scope: :restaurant_id }
 
+  scope :cheap, -> { where('price < 500') }
+
   def to_s
     name
   end
