6f252f62dd8fb6e7f23c894cf13520b4827cb157
Jason Swett <jason@benfranklinlabs.com>
2018-06-18 07:53:46 -0400

    Add seed data, add price to menu item index page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+jonathans = Restaurant.create!(
+  name: "Jonathan's",
+  phone: '(111) 222-3333'
+)
+
+sy_thai = Restaurant.create!(
+  name: 'Sy Thai',
+  phone: '(844) 828-8388'
+)
+
+slows = Restaurant.create!(
+  name: "Slow's",
+  phone: '(537) 994-3833'
+)
+
+MenuItem.create!([
+  {
+    restaurant: jonathans,
+    name: 'Hot Dog',
+    price: 300
+  },
+  {
+    restaurant: jonathans,
+    name: 'Chili Dog',
+    price: 450
+  },
+  {
+    restaurant: jonathans,
+    name: 'Chicago Dog',
+    price: 450
+  },
+  {
+    restaurant: sy_thai,
+    name: 'Pad Thai',
+    price: 800
+  },
+  {
+    restaurant: sy_thai,
+    name: 'Drunken Noodle',
+    price: 800
+  },
+  {
+    restaurant: sy_thai,
+    name: 'Egg Noodle',
+    price: 800
+  },
+  {
+    restaurant: slows,
+    name: 'Pulled Pork',
+    price: 1000
+  },
+  {
+    restaurant: slows,
+    name: 'Beef Brisket',
+    price: 1200
+  },
+])
